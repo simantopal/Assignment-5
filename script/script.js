6b3371cf4c7f5296519023443899c562eac9dedf
simantopal <simantop13@gmail.com>
2026-03-07 02:19:36 +0600

    Create login page

---
+++ script/script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "admin123"){
+        localStorage.setItem("isLoggedIn", true);
+        window.location.href = "index.html";
+    }else{
+        alert("Invalid credentials");
+    }
+});
