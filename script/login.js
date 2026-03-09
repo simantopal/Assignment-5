4c009bf6395babc381919dfafc7fe40a373e8019
simantopal <simantop13@gmail.com>
2026-03-09 13:54:18 +0600

    login issue correction

--- script/login.js
+++ script/login.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("loginForm");
 
-form.addEventListener("submit", function(login){
-    login.preventDefault();
+form.addEventListener("submit", function(e){
+    e.preventDefault();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
